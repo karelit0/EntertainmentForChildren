9cab929e232e85b25b21ef7d849d2fea8bc694c2
Daniel Marcelo Maldonado Sanchez <karelit0@users.noreply.github.com>
2020-05-19 15:35:55 -0500

    Update player.sprite.ts

--- Phaser/src/app/game-tools/player.sprite.ts
+++ Phaser/src/app/game-tools/player.sprite.ts
@@ -2,34 +2,75 @@ import Phaser from 'phaser';
 
 export class PlayerSprite extends Phaser.Physics.Arcade.Sprite {
 
-    loadCharacterImageAssets(characterName: string, characterState: string, imagesToLoad: number) {
-        for (let i = 0; i < imagesToLoad; i++) {
-            const imageKey: string = characterName + '_' + characterState + '_00' + i;
-            const imageFile: string = '/assets/character/' + characterName + '/' + characterState + '/' + characterState + '__00' + i + '.png';
-            this.scene.load.image(imageKey, imageFile);
+    cursors: Phaser.Types.Input.Keyboard.CursorKeys;
+    isBusy: boolean;
+    hitGround: boolean;
+
+    /**
+     * 
+     * @param scene The Scene to which this Game Object belongs. A Game Object can only belong to one Scene at a time.
+     * @param x The horizontal position of this Game Object in the world.
+     * @param y The vertical position of this Game Object in the world.
+     * @param texture The key of the Texture this Game Object will use to render with, as stored in the Texture Manager.
+     * @param frame An optional frame from the Texture this Game Object is rendering with.
+     */
+    constructor(scene: Phaser.Scene, x: number, y: number, texture: string, frame?: string | integer) {
+        super(scene, x, y, texture, frame);
+
+        this.cursors = this.scene.input.keyboard.createCursorKeys();
+
+        scene.physics.add.existing(this);
+        this.isBusy = false;
+        this.hitGround = false;
+    }
+
+    update() {
+        this.updatePlayer();
+    }
+
+    onHitPlatform() {
+        this.hitGround = true;
+    }
+
+    private updatePlayer() {
+
+        if (this.cursors.up.isDown || this.cursors.down.isDown || this.cursors.left.isDown || this.cursors.right.isDown) {
+            this.isBusy = true;
+            if (this.cursors.up.isDown || this.cursors.down.isDown) {
+                this.hitGround = false;
+                if (this.cursors.up.isDown) {
+                    this.setVelocityY(-150);
+                } else {
+                    this.setVelocityY(150);
+                }
+            }
+
+            if (this.cursors.left.isDown || this.cursors.right.isDown) {
+                if (this.cursors.left.isDown) {
+                    if (!this.flipX) {
+                        this.setFlip(true, false);
+                    }
+                    this.setVelocityX(-160);
+                } else {
+                    this.setFlip(false, false);
+                    this.setVelocityX(160);
+
+                }
+            }
+        } else {
+            this.isBusy = false;
+        }
+
+        if (this.hitGround) {
+            if (this.isBusy) {
+                this.anims.play('ninjaGirl_Run', true);
+            } else {
+                this.anims.play('ninjaGirl_Idle', true);
+                this.setVelocityX(0);
+            }
+        } else {
+            this.anims.play('ninjaGirl_Jump', true);
         }
     }
 
-    createCharacterAnimation(characterName: string, characterState: string, framesToLoad: number) {
-
-        const frames: Phaser.Types.Animations.AnimationFrame[] = [];
-
-        const characterAnimationKey: string = characterName + '_' + characterState;
-
-        for (let i = 0; i < framesToLoad; i++) {
-            const frameKey: string = characterName + '_' + characterState + '_00' + i;
-
-            frames.push({
-                key: frameKey,
-                frame: null
-            });
-        }
-
-        this.scene.anims.create({
-            key: characterAnimationKey,
-            frames: frames,
-            frameRate: 10,
-            repeat: -1
-        });
-    }
 }
